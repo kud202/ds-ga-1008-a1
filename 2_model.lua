4d5df361ff278435761c015cc76cb24af8cf8cef
Yijun Xiao <ryjxiao@nyu.edu>
2016-01-27 21:35:12 -0500

    modification

--- 2_model.lua
+++ 2_model.lua
@@ -80,19 +80,18 @@ elseif opt.model == 'convnet' then
 
       -- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
       model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
-      model:add(nn.ReLU())
+      model:add(nn.Tanh())
       model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
 
       -- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
       model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
-      model:add(nn.ReLU())
+      model:add(nn.Tanh())
       model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
 
       -- stage 3 : standard 2-layer neural network
       model:add(nn.View(nstates[2]*filtsize*filtsize))
-      model:add(nn.Dropout(0.5))
       model:add(nn.Linear(nstates[2]*filtsize*filtsize, nstates[3]))
-      model:add(nn.ReLU())
+      model:add(nn.Tanh())
       model:add(nn.Linear(nstates[3], noutputs))
 
    else
